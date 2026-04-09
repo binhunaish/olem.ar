ac8771dc0525991e802b76a20b2f6109237c0726
mohamed <mhyahoo2006@gmail.com>
2026-04-10 00:38:46 +0200

    feat: integrate Tailwind CSS and update styles
    - Added Tailwind CSS as a dependency and configured PostCSS to use Tailwind and Autoprefixer.
    - Created a new PostCSS configuration file.
    - Updated custom CSS to include Tailwind's base, components, and utilities.
    - Modified primary color variables for light and dark themes.
    - Removed deprecated navbar styles to streamline the design.
    - Added a Tailwind configuration file to specify content paths for purging unused styles.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './docs/**/*.{md,mdx}',
+    './blog/**/*.{md,mdx}',
+    './basics-doc/**/*.{md,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

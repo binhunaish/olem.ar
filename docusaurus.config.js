d86d129e31a9673d56c902444ab30203287c3ccc
mohamed <mhyahoo2006@gmail.com>
2026-08-16 03:43:34 +0200

    refactor: clean install step added to GitHub Actions workflow and update Docusaurus config for consistency

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,65 +1,66 @@
-import { themes as prismThemes } from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 import fs from "fs";
 import path from "path";
 
 // get Docs Instances from the "docs" directory
 function getDocsInstances() {
-  const docsPath = path.join(__dirname, 'docs');
+  const docsPath = path.join(__dirname, "docs");
   if (!fs.existsSync(docsPath)) return [];
 
-  return fs.readdirSync(docsPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name);
+  return fs
+    .readdirSync(docsPath, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 }
 
 // Create plugin instances for each docs instance for Docusaurus
-const docsPlugins = getDocsInstances().map(instanceId => [
-  '@docusaurus/plugin-content-docs',
+const docsPlugins = getDocsInstances().map((instanceId) => [
+  "@docusaurus/plugin-content-docs",
   {
     id: instanceId,
     path: `docs/${instanceId}`,
-    routeBasePath: `docs/${instanceId}`
-  }
+    routeBasePath: `docs/${instanceId}`,
+  },
 ]);
 
 const config = {
   // Docusaurus Configuration for the project
-  title: 'عُلِم',
-  tagline: 'منصة تعليمية مفتوحة المصدر',
-  favicon: 'img/favicon.ico',
+  title: "عُلِم",
+  tagline: "منصة تعليمية مفتوحة المصدر",
+  favicon: "img/favicon.ico",
   future: {
     v4: true,
   },
-  url: 'https://binhunaish.github.io/',
-  baseUrl: '/olem.ar',
-  organizationName: 'عُلِم',
-  projectName: 'olem.ar',
-  onBrokenLinks: 'throw',
-  onBrokenAnchors: 'ignore',
+  url: "https://binhunaish.github.io/",
+  baseUrl: "/olem.ar",
+  organizationName: "عُلِم",
+  projectName: "olem.ar",
+  onBrokenLinks: "throw",
+  onBrokenAnchors: "ignore",
 
   // language settings for Arabic with RTL support
   i18n: {
-    defaultLocale: 'ar',
-    locales: ['ar'],
+    defaultLocale: "ar",
+    locales: ["ar"],
     localeConfigs: {
       ar: {
-        direction: 'rtl',
+        direction: "rtl",
       },
-    }
+    },
   },
 
   // the default setting of Docusaurus
   presets: [
     [
-      'classic',
-      ({
+      "classic",
+      {
         docs: false,
         blog: false,
         theme: {
           // custom CSS file for the project
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
-      }),
+      },
     ],
   ],
 
@@ -82,8 +83,7 @@ const config = {
   ],
 
   // theme configuration for the project
-  themeConfig:
-  {
+  themeConfig: {
     image: "/img/logo.svg",
     colorMode: {
       respectPrefersColorScheme: false,
@@ -91,8 +91,15 @@ const config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-    }
-  }
+    },
+  },
+
+  /*markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+      onBrokenMarkdownImages: "warn",
+    },
+  },*/
 };
 
 export default config;
